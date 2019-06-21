3db7c1c41df26f275375e9fb872b1a7f9c54ce01
Melecoco <duhalgouetm@yahoo.com>
2019-06-21 19:41:21 +0200

    templatingFilmList finished

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import './App.css';
 import FilmList from './components/FilmList.js';
 
 let testTableau = [{id: 1, title: 'test', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 2, title: 'test', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 3, title: 'test', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 4, title: 'test', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 5, title: 'test', releaseYear: 'test', synopsis: 'test', poster: ''}]
+{id: 2, title: 'Harry', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 3, title: 'Potter', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 4, title: 'Star', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 5, title: 'WARs', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 6, title: 'héhé', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 7, title: 'eze', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 8, title: 'bonjour', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 9, title: 'grand-pere', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 10, title: 'ohoh pere noel', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 11, title: '?????', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 12, title: 'Nihao', releaseYear: 'test', synopsis: 'test', poster: ''},
+{id: 13, title: 'XaiXien', releaseYear: 'test', synopsis: 'test', poster: ''}]
 
 function App() {
   return (
