ed405d05d021c77d07e738fb455b16f92aeb5c5d
Unknown <duhalgouetm@yahoo.com>
2019-06-24 12:08:50 +0200

    beginning designing detailFilm with css

--- src/views/DetailMovieView.js
+++ src/views/DetailMovieView.js
@@ -2,7 +2,8 @@ import React, {Component} from "react";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import {getBackdropFromApi, getFilmDetailFromApi, getImageFromApi} from "../API/TMDB";
+import {getBackdropFromApi, getFilmDetailFromApi, getPosterFromApi} from "./../API/TMDB";
+import './viewsCss/DetailMovieView.css'
 
 
 // import "../Helpers/App.css"
@@ -32,6 +33,40 @@ export class DetailMovieView extends Component {
     }
 
     render(){
-        return(<div>vide</div>)
+        const { title, genres, overview, poster_path, production_companies, release_date, backdrop_path } = this.state;
+        return(
+        <div className="App">
+            {/* <div>
+                <img src={getBackdropFromApi(backdrop_path)}/>
+            </div> */}
+            <h1>{title}</h1>
+            <div>
+                {genres.map((genre, key) => 
+                    {
+                        return`${genre.name} `
+                    })
+                }
+            </div>            
+            <div>
+                <p>{overview}</p>
+            </div>
+            <div>
+                <img src={getPosterFromApi(poster_path)}/>
+            </div>
+            <div>
+                <h2>
+                    produced by: 
+                    {production_companies.map((companie) =>
+                    {
+                        return `${companie.name}`
+                    })}
+                </h2>
+                <h2>released in {release_date}</h2>
+                
+            </div>
+            
+            
+        </div>
+        )
     }
 }
