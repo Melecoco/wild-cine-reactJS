f18df95edd405919a4d034ee933163d1c45d2075
Unknown <duhalgouetm@yahoo.com>
2019-06-24 15:49:32 +0200

    first Ideas design/css

--- src/views/DetailMovieView.js
+++ src/views/DetailMovieView.js
@@ -31,6 +31,17 @@ export class DetailMovieView extends Component {
         return response;
 
     }
+   
+    
+
+    isDataEmpty(data){
+        if(data == ''){            
+            return "nothing to display"
+        }
+        else{
+            return data
+        }
+    }
 
     render(){
         const { title, genres, overview, poster_path, production_companies, release_date, backdrop_path } = this.state;
@@ -39,31 +50,56 @@ export class DetailMovieView extends Component {
             {/* <div>
                 <img src={getBackdropFromApi(backdrop_path)}/>
             </div> */}
-            <h1>{title}</h1>
-            <div>
-                {genres.map((genre, key) => 
-                    {
-                        return`${genre.name} `
-                    })
-                }
-            </div>            
-            <div>
-                <p>{overview}</p>
-            </div>
-            <div>
-                <img src={getPosterFromApi(poster_path)}/>
-            </div>
-            <div>
-                <h2>
-                    produced by: 
-                    {production_companies.map((companie) =>
-                    {
-                        return `${companie.name}`
-                    })}
-                </h2>
-                <h2>released in {release_date}</h2>
-                
-            </div>
+            <main className='main'>
+                <div className='detail-infos'>
+                    <div className='detail-container titre'>
+                        <h6>TITRE</h6>
+                        <h1>{this.isDataEmpty(title)}</h1>
+                    </div>
+                    
+                    <div  className='detail-container genre'>
+                        <h6>GENRE</h6>
+                        <h1>
+                            {genres.map((genre, key) => 
+                                {
+                                    return`${genre.name} `
+                                })
+                            }
+                        </h1>
+                    </div>
+
+                    <div className='detail-container producers'>
+                        <h6>PRODUCTION</h6>
+                        <h1> 
+                            {production_companies.map((companie) =>
+                            {
+                                return `${companie.name}`
+                            })}
+                        </h1>
+                    </div>
+                    <div className='detail-container releaseYear'>
+                            
+                            <h6>RELEASED YEAR</h6>
+                            <h1>
+                            {this.isDataEmpty(release_date)}
+                            </h1>
+                    </div>
+                    <div className='detail-container synopsis'>
+                        <h6>SYNOPSIS</h6>
+                        <div>{this.isDataEmpty(overview)}</div>
+                    </div>
+                    
+                </div>
+
+                <div className='poster'>
+                    <img src={getPosterFromApi(poster_path)}/>
+                </div>
+            </main>
+            
+            
+
+            
+            
             
             
         </div>
