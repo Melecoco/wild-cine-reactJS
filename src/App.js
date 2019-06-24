029f3b936ee05a94704e2ffe9cecbf2db28cca48
Unknown <duhalgouetm@yahoo.com>
2019-06-24 10:23:30 +0200

    searcher feature is working

--- src/App.js
+++ src/App.js
@@ -2,27 +2,12 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FilmList from './components/FilmList.js';
+import Searcher from './components/Searcher'
 import {getMostPopularFilms} from './API/TMDB'
-
-let testTableau = [{id: 1, title: 'test', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 2, title: 'Harry', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 3, title: 'Potter', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 4, title: 'Star', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 5, title: 'WARs', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 6, title: 'héhé', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 7, title: 'eze', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 8, title: 'bonjour', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 9, title: 'grand-pere', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 10, title: 'ohoh pere noel', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 11, title: '?????', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 12, title: 'Nihao', releaseYear: 'test', synopsis: 'test', poster: ''},
-{id: 13, title: 'XaiXien', releaseYear: 'test', synopsis: 'test', poster: ''}]
-
 
 class App extends Component {
 
-  state = {
-    
+  state = { 
     list: []
   }
 
@@ -30,14 +15,17 @@ class App extends Component {
     this.setState( {list : await getMostPopularFilms()})
   }
 
+  sendToState(list){
+    this.setState({list})
+  }
+
   componentDidMount(){
-    console.log(getMostPopularFilms())
-    
     this.getDataFilm()
   }
   render(){
     return (
       <div className="App">
+        <Searcher onComplete={this.sendToState.bind(this)}></Searcher>
         <FilmList items={this.state.list}></FilmList>
       </div>
     );
