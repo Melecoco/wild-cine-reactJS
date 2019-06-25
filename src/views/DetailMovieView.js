0b9f1a86d314fbf06dd548773b7768d1c74b4708
Unknown <duhalgouetm@yahoo.com>
2019-06-25 12:09:30 +0200

    bonusFeature  ChangeColorForFun

--- src/views/DetailMovieView.js
+++ src/views/DetailMovieView.js
@@ -17,22 +17,40 @@ export class DetailMovieView extends Component {
         overview: null,
         poster_path: null,
         production_companies: [],
-        release_date: null
+        release_date: null,
+        backGroundColor: 'lightseagreen',
+        borderColor: 'lightseagreen',
+        colorError: '',
     }
 
     componentDidMount() {
         this.getFilm(this.props.match.params.movieId);
+        console.log('hey')
     }
 
     async getFilm(id) {
         const response = await getFilmDetailFromApi(id);
-
         this.setState(response);
         return response;
 
     }
    
-    
+    handleChangeColor(e){
+        
+        this.setState({
+            backGroundColor: e.target.value,
+            borderColor: e.target.value
+        }, function(){
+            this.validateColor()
+        })       
+    }
+    validateColor(){
+        const {backgroundColor, borderColor} = this.state
+        if(backgroundColor || borderColor === 'lightseagreen' || 'lightcoral' || 'lightgreen'){
+            this.setState({ colorError: "color can't be something else" });
+        }
+    }
+
 
     isDataEmpty(data){
         if(data == ''){            
@@ -52,12 +70,14 @@ export class DetailMovieView extends Component {
             </div> */}
             <main className='main'>
                 <div className='detail-infos'>
-                    <div className='detail-container titre'>
+                    <div className='detail-container titre' 
+                    style={{backgroundColor: `${this.state.backGroundColor}`,
+                    borderColor: `${this.state.borderColor}`}}>
                         <h6>TITRE</h6>
                         <h1>{this.isDataEmpty(title)}</h1>
                     </div>
                     
-                    <div  className='detail-container genre'>
+                    <div  className='detail-container genre' style={{borderColor: `${this.state.borderColor}`}}>
                         <h6>GENRE</h6>
                         <h1>
                             {genres.map((genre, key) => 
@@ -68,7 +88,7 @@ export class DetailMovieView extends Component {
                         </h1>
                     </div>
 
-                    <div className='detail-container producers'>
+                    <div className='detail-container producers' style={{borderColor: `${this.state.borderColor}`}}>
                         <h6>PRODUCTION</h6>
                         <h1> 
                             {production_companies.map((companie) =>
@@ -77,21 +97,31 @@ export class DetailMovieView extends Component {
                             })}
                         </h1>
                     </div>
-                    <div className='detail-container releaseYear'>
+                    <div className='detail-container releaseYear' style={{borderColor: `${this.state.borderColor}`}}>
                             
                             <h6>RELEASED YEAR</h6>
                             <h1>
                             {this.isDataEmpty(release_date)}
                             </h1>
                     </div>
-                    <div className='detail-container synopsis'>
+                    <div className='detail-container synopsis' style={{borderColor: `${this.state.borderColor}`}}>
                         <h6>SYNOPSIS</h6>
                         <div>{this.isDataEmpty(overview)}</div>
                     </div>
                     
                 </div>
 
-                <div className='poster'>
+                <div className='poster' style={{backgroundColor: `${this.state.backGroundColor}`, 
+                borderColor: `${this.state.borderColor}`}}>
+                    <div>
+                        <h5>Change color for fun</h5>
+                        <select onChange={(e)=>this.handleChangeColor(e)}>
+                        <option value="lightseagreen">bleu turquoise</option>
+                        <option value="lightcoral">rose corail</option>
+                        <option value="lightgreen">vert pelouse</option>
+                    </select>
+                    </div>
+                    
                     <img src={getPosterFromApi(poster_path)}/>
                 </div>
             </main>
